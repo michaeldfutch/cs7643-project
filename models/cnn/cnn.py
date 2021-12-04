7730ad15626e6706630dd087b311d213f0d2e063
Michael Futch <mfutch3@gatech.edu>
2021-12-04 14:08:08 -0500

    initial training notebook

--- models/cnn/cnn.py
+++ models/cnn/cnn.py
@@ -1,8 +1,21 @@
-import math
-import time
-import numpy as np
-
-# Pytorch packages
 import torch
-import torch.optim as optim
 import torch.nn as nn
+
+# using vanilla CNN from assignment 2 as a starting point
+class VanillaCNN(nn.Module):
+    def __init__(self):
+        super(VanillaCNN, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=3,out_channels=32, kernel_size=7, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten(1),
+            nn.Linear(5408,out_features=10)
+        )
+
+
+    def forward(self, x):
+        outs = None
+        x = torch.reshape(x,(x.shape[0], 3, 32, 32))
+        outs = self.model(x)
+        return outs
